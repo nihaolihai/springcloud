288bf3471395b1385bcec4d6d8afd3a8f5f151a5
tfqiu <qIulovejun889>
2021-02-28 16:07:59 +0800

    添加nacos集群

--- cloud-nacos-config-3377/src/main/java/com/demo/springcloud/controller/NacosConfigController.java
+++ cloud-nacos-config-3377/src/main/java/com/demo/springcloud/controller/NacosConfigController.java
@@ -1,14 +1,17 @@
 package com.demo.springcloud.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope//支持nacos动然刷新功能
+@RequestMapping("test/api")
 public class NacosConfigController {
 
-    //@Value("${config.info}")
+    @Value("${config.info}")
     private String serverinfo;
 
     @GetMapping(value = "/configinfo")
